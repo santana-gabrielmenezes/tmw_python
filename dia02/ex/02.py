ce43e442ad69c3c9f83d6c2c5b197f4157f282a7
santana.gabrielmenezes <santana.gabrielmenezes+github@gmail.com>
2025-08-22 05:29:50 -0300

    até 1:10:00 da live do dia 02 do Teo Me Why sobre Pyton

---
+++ dia02/ex/02.py
@@ -0,0 +1,7 @@
+# Faça um programa para calcular a raiz quadrada de um número e exibir o resultado
+
+numero = int(input("Digite o número a ser calculado a raiz quadrada: "))
+
+raiz = numero ** (1/2)
+
+print("A raiz de ", numero, " é ", round(raiz))
